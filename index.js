18b589498559567f6dc2992b24f29779886e59da
Aditya Upadhyay <adi@iitj.ac.in>
2016-04-23 14:06:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,31 +11,6 @@ module.exports = {
 
   contentFor: function(type, config) {
     var content = '';
-
-    if (type === 'head') {
-      var src = "//maps.googleapis.com/maps/api/js";
-      var gMapConfig = config['g-map'] || {};
-      var params = [];
-
-      var key = gMapConfig.key;
-      if (key) {
-        params.push('key=' + encodeURIComponent(key));
-      }
-
-      var libraries = gMapConfig.libraries;
-      if (libraries && libraries.length) {
-        params.push('libraries=' + encodeURIComponent(libraries.join(',')));
-      }
-
-      var protocol = gMapConfig.protocol;
-      if (protocol) {
-        src = protocol + ":" + src;
-      }
-
-      src += '?' + params.join('&');
-      content = '<script type="text/javascript" src="' + src + '"></script>';
-    }
-
     return content;
   }
 };
